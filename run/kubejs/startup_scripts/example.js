1bdd7370187d9137812b5ffd4822dcb4c45595be
Kli Kli <klikli@outlook.at>
2024-10-27 10:53:15 +0100

    chore: add usage note for new theurgy feature

--- run/kubejs/startup_scripts/example.js
+++ run/kubejs/startup_scripts/example.js
@@ -8,6 +8,10 @@
 
 console.info('Hello, World! (Loaded startup scripts)')
 
+//NOTE: In order for tooltips to work, the namespace of the item must be registered with the theurgy tooltip handler.
+//      This can be done in code (with a mod) using: TooltipHandler.registerNamespaceToListenTo("<my_namespace>");
+//      Or via config in theurgy-server.toml (either in ./defaultconfigs or ./saves/<world>/serverconfigs) in the section [tooltipHandler] with the key additionalTooltipHandlerNamespaces = ["<my_namespace>"] (If you load into a world once, an empty config is created for you that you can edit and copy into ./defaultconfigs for your pack.
+
 StartupEvents.registry('item', (event) => {
 	event.create('a_test_sulfur', 'theurgy:alchemical_sulfur')
 		.sourceItem('minecraft:rotten_flesh')
