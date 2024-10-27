00141adafafd183d4072754d856ca8ad3993dda1
Kli Kli <klikli@outlook.at>
2024-10-27 10:56:42 +0100

    chore: add usage note for new theurgy feature

--- runs/server/kubejs/startup_scripts/example.js
+++ runs/server/kubejs/startup_scripts/example.js
@@ -8,11 +8,15 @@
 
 console.info('Hello, World! (Loaded startup scripts)')
 
+//NOTE: In order for tooltips to work, the namespace of the item must be registered with the theurgy tooltip handler.
+//      This can be done in code (with a mod) using: TooltipHandler.registerNamespaceToListenTo("<my_namespace>");
+//      Or via config in theurgy-server.toml (either in ./defaultconfigs or ./saves/<world>/serverconfigs) in the section [tooltipHandler] with the key additionalTooltipHandlerNamespaces = ["<my_namespace>"] (If you load into a world once, an empty config is created for you that you can edit and copy into ./defaultconfigs for your pack.
+
 StartupEvents.registry('item', (event) => {
     event.create('a_test_sulfur', 'theurgy:alchemical_sulfur')
         .sourceItem('minecraft:rotten_flesh')
         .sourceName("Rotten Flesh")
-        .sulfurTier("abundant")
+        .derivativeTier("abundant")
         .sulfurType("misc")
 })
 
@@ -20,5 +24,5 @@ StartupEvents.registry('item', (event) => {
     event.create('a_test_niter', 'theurgy:alchemical_niter')
         .niterIcon('minecraft:spider_eye') //ideally an item with a custom icon should be used here. Theurgy e.g. uses the item "gems_abundant_icon" for the abundant gems niter
         .sourceName("Monster Parts")
-        .sulfurTier("common")
+        .derivativeTier("common")
 })
